f68e2fcddb323e8c3495a21ce910ab600ae476e5
ju3tin <ju3tin@hotmail.co.uk>
2025-01-23 08:41:48 +0000

    ewr

--- index.js
+++ index.js
@@ -28,10 +28,35 @@ const wss = new WebSocket.Server({ server }, () => {
 wss.on('connection', (ws) => {
   console.log('Client connected.');
 
+  clients.set(ws, {
+    isBetted: false,
+    isTook: false,
+    cntBalance: 100,
+    cntBet: 0,
+});
+
   ws.on('message', async (message) => {
-    const data = JSON.parse(message);
+    try {
+      const playerState = clients.get(ws);
+      const data = JSON.parse(message);
 
     switch (data.type) {
+      case 'BTN_BET_CLICKED':
+        if (!playerState.isBetted && isGameRunning) {
+            // Player places a bet
+            playerState.cntBet = Math.min(data.bet, playerState.cntBalance);
+            playerState.isBetted = true;
+            playerState.cntBalance -= playerState.cntBet;
+            ws.send(JSON.stringify({ action: 'BET_MADE', player: data.walletAddress, bet: data.bet, currency: data.currency}))
+            ws.send(JSON.stringify({ action: 'CNT_BALANCE', balance: playerState.cntBalance.toFixed(2) }));
+        } else if (playerState.isBetted && isGameRunning && !playerState.isTook) {
+            // Player cashes out
+            playerState.cntBalance += playerState.cntBet * currentMultiplier;
+            playerState.isTook = true;
+            ws.send(JSON.stringify({ action: 'WON', bet: playerState.cntBet, mult: currentMultiplier.toFixed(2) }));
+        }
+        await GameController.handleBet(ws, data);
+        break;
       case 'PLACE_BET':
         await GameController.handleBet(ws, data);
         break;
@@ -48,6 +73,10 @@ wss.on('connection', (ws) => {
         ws.send(JSON.stringify({ action: 'ERROR', message: 'Unsupported action.' }));
         break;
     }
+  } catch (error) {
+    console.error('Error processing message:', error);
+    ws.send(JSON.stringify({ action: 'ERROR', message: 'An error occurred while processing the message.' }));
+  }
   });
 
   ws.on('close', () => console.log('Client disconnected.'));
